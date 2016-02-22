e3f11cede60632e88d86d1db0baaa274be6b2981
delihiros <delihiros@gmail.com>
2016-02-23 04:18:55 +0900

    add speech synthesis

--- speech_synthesis.py
+++ speech_synthesis.py
@@ -1,4 +1,17 @@
+# -*- encoding: UTF-8 -*-
+
+from naoqi import ALProxy
+
 class SpeechSynthesizer:
+    def __init__(self, ip="192.168.43.171"):
+        self.tts = ALProxy("ALTextToSpeech", ip, 9559)
+        self.tts.setLanguage("Japanese")
+
     def synthesize(self, response):
         print("MACHINE:",response)
+        tts.say(response)
         return response
+
+def __main__(self):
+    speech_synthesizer = SpeechSynthesizer()
+    speech_synthesizer.synthesize("こんにちは！")
