d456c79409a37d8b0b0537b399dad81ff5a3293f
delihiros <delihiros@gmail.com>
2016-02-24 02:33:48 +0900

    add speech recognizer

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 # how to use
 
+```
 $ python trapepper/ASRServer.py # ASR server on localhost:8000
 $ python trapepper/dialogue.py
+```
+
+# making audio file
+
+```
+$ mpg123 -w nara.wav nara.mp3
+$ sox nara.wav -c 1 -r 16000 nara_1chann.wav
+```
--- speech_recognizer.py
+++ speech_recognizer.py
@@ -1,4 +1,5 @@
 import sys, re
+import pyaudio, wave
 import requests
 
 from trapepper.util import log
@@ -9,14 +10,46 @@ class SpeechRecognition:
         self.debug = debug
         self.s = requests.Session()
 
+    def record(self):
+        print("recording...")
+        chunk = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 1
+        RATE = 16000
+        RECORD_SECONDS = 7
+        
+        p = pyaudio.PyAudio()
+        
+        stream = p.open(
+                format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                output=True,
+                frames_per_buffer=chunk)
+        
+        frames = []
+        for i in range(0, int(RATE / chunk * RECORD_SECONDS)):
+            print("recording... ", str(i), "/", int(RATE / chunk * RECORD_SECONDS), "\r")
+            data = stream.read(chunk)
+            frames.append(data)
+        
+        wf = wave.open("./trapepper/sample.wav", "wb")
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+
     def recognize(self):
         if self.debug:
             return input("User: ")
         else:
-            url = "http://localhost:8000/asr_julius"
             url = "http://192.168.43.29:8000/asr_julius"
+            self.record()
             files = {
-                    'myFile': open('./trapepper/resources/nara_1chann.wav', 'rb')
+                    'myFile': open('./trapepper/sample.wav', 'rb')
                     }
             r = self.s.post(url, files=files)
             line = r.text.split('\n')[0].split(':')[-1].replace(' ', '')
